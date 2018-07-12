03ba53241cb8aa1ae55694cb2ceeb94c42211778
Charlie Getzen <charlie.getzen@procore.com>
2018-07-12 14:58:01 -0700

    output

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,6 @@
 wget $SMENV
 dpkg -i smenv_0.0.1_amd64.deb
 smenv -s $AWS_SECRETS_ENTRY /config.sh
+cat /etc/pgbouncer/pgbouncer.ini
 service pgbouncer start
 tail -f /var/log/postgresql/pgbouncer.log
